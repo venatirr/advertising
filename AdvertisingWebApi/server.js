8c7c037a695b44d409ae0b6cbe9502fea6c07b21
Florin Capra <fcapra@ezetop.com>
2016-06-08 09:25:32 +0300

    http get example

--- AdvertisingWebApi/server.js
+++ AdvertisingWebApi/server.js
@@ -1,1 +1,17 @@
-test
+var http = require('http');
+var url = 'http://api.digicelgroup.com:8802/countries?productTypes=Data&productCurrencyIso=';
+
+http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var fbResponse = JSON.parse(body);
+        console.log("Got a response: ", fbResponse[0]);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
